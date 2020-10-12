6e2fc4609bacc88e3ea7ba5da8ae4ac33bb9ee8a
aherrera3 <angelicaherreraalba@gmail.com>
2020-10-11 22:32:38 -0500

    avance en desintegracion de neutron

--- main.py
+++ main.py
@@ -36,15 +36,16 @@ def Reset(r):
 
 rButton = button(text="Reset", pos=scene.title_anchor, bind=Reset)    
 
+
+# PARTE LOGICA
+"""
+#dos partículas en el eje x
+
 def estado_inicial():
     t=0
     electron, electron_nombre = mod.electron(vector(-15,0,0), vector(1,0,0))
     muon, muon_nombre = mod.muon(vector(15,0,0), vector(-1,0,0))
 
-
-
-# PARTE LOGICA
-#dos partículas en el eje x
 electron, electron_nombre = mod.electron(vector(-15,0,0), vector(1,0,0))
 muon, muon_nombre = mod.muon(vector(15,0,0), vector(-1,0,0))
 
@@ -68,6 +69,7 @@ while (not colision):
         muon.visible = False
         colision = True
 
+# choque aleatorio
 nballs = np.random.randint(2,5) #numero de partículas resultantes del choque
 colores = [color.white, color.yellow, color.blue, color.green, color.red] #coleccion de colores
 balls = []
@@ -76,16 +78,70 @@ balls = []
 for i in range(nballs):
 	balls.append(sphere(pos=vector(0,0,0), radius=1, color=np.random.choice(colores), make_trail=True, shininess=0, masa=10, velocidad=np.random.rand(3)*2-1))
 
-t=0
+
 #despues del choque
-while True:
+t = 0
+t_vida = 10    # tiempo de vida
+stop = False
+while (not stop):
 	rate(20)
 	for b in balls:
 		b.pos.x=b.pos.x + b.velocidad[0]*dt
 		b.pos.y=b.pos.y + b.velocidad[1]*dt
 		b.pos.z=b.pos.z + b.velocidad[2]*dt
         
-	t+=dt
-	if t>10:
-		break
+	t += dt
+	if t >= t_vida:
+		stop = True
+"""
 
+# desintegracion
+
+def estado_inicial():
+    t=0
+    neutron = mod.neutron(vector(-15,0,0), vector(1,0,0), 0.5)
+
+neutron = mod.neutron(vector(-15,0,0), vector(1,0,0), 0.5)
+
+t = 0
+dt = 0.1
+vel_neutron = 1
+pos_desintegracion = -1
+t_desintegracion = 10
+
+#antes del choque
+desintegracion = False
+while (not desintegracion):
+    rate(20) #fps
+    neutron.evolucion_temporal(dt)
+    
+    t = t + dt
+    if t >= t_desintegracion: # cuando se desintegra
+        neutron.eliminar()
+        desintegracion = True
+"""
+# choque aleatorio
+nballs = np.random.randint(2,5) #numero de partículas resultantes del choque
+colores = [color.white, color.yellow, color.blue, color.green, color.red] #coleccion de colores
+balls = []
+
+#crea y almacena las partículas con un rango de velocidades aleatorias
+for i in range(nballs):
+	balls.append(sphere(pos=vector(0,0,0), radius=1, color=np.random.choice(colores), make_trail=True, shininess=0, masa=10, velocidad=np.random.rand(3)*2-1))
+
+
+#despues del choque
+t = 0
+t_vida = 10    # tiempo de vida
+stop = False
+while (not stop):
+	rate(20)
+	for b in balls:
+		b.pos.x=b.pos.x + b.velocidad[0]*dt
+		b.pos.y=b.pos.y + b.velocidad[1]*dt
+		b.pos.z=b.pos.z + b.velocidad[2]*dt
+        
+	t += dt
+	if t >= t_vida:
+		stop = True
+"""
