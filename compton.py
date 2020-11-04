26b639df82ba7a359cc09d3614869a792ec1fc26
David <jd.salamancac>
2020-11-04 17:41:03 -0500

    foton funcional

--- compton.py
+++ compton.py
@@ -37,50 +37,58 @@ def estado_inicial():
     #running=False
 
 
+datos=np.loadtxt("longitudes_de_onda.csv",dtype=int)
 
-def longitud_de_onda(l):
-    r=0.0 
-    g=0.0
-    b=0.0
+conversion={}
 
-    if ((l>=400.0) and (l<410.0)):
-        t=(l-400.0)/(410.0-400.0)
-        r+=(0.33*t)-(0.20*t*t)
-    elif ((l>=410.0) and (l<475.0)):
-        t=(l-410.0)/(475.0-410.0)
-        r=0.14-(0.13*t*t)
-    elif ((l>=545.0) and (l<595.0)):
-        t=(l-545.0)/(595.0-545.0)
-        r=(1.98*t)-(t*t)
-    elif ((l>=595.0) and (l<650.0)):
-        t=(l-595.0)/(650.0-595.0) 
-        r=0.98+(0.06*t)-(0.40*t*t)
-    elif ((l>=650.0) and (l<700.0)):
-        t=(l-650.0)/(700.0-650.0)
-        r=0.65-(0.84*t)+(0.20*t*t)
-    if ((l>=415.0) and (l<475.0)):
-        t=(l-415.0)/(475.0-415.0) 
-        g=(0.80*t*t)
-    elif ((l>=475.0) and (l<590.0)):
-        t=(l-475.0)/(590.0-475.0)
-        g=0.8 +(0.76*t)-(0.80*t*t)
-    elif ((l>=585.0) and (l<639.0)): 
-        t=(l-585.0)/(639.0-585.0)
-        g=0.84-(0.84*t)
-    if ((l>=400.0) and (l<475.0)):
-        t=(l-400.0)/(475.0-400.0)
-        b=(2.20*t)-(1.50*t*t)
-    elif ((l>=475.0) and(l<560.0)) :
-        t=(l-475.0)/(560.0-475.0)
-        b=0.7-t+(0.30*t*t)
-    return r,g,b
-foton=helix(pos=vector(0,0,0), axis=vector(5,0,0), radius=0.5,color=vector(0,0,0))
+for i in datos:
+    conversion[str(i[0])] = (i[1],i[2],i[3])
 
-elipse=ellipsoid(pos=vector(2.5,0,0),axis=vector(5,0,0), length=6, height=2, width=2,color=vector(250,0,0),opacity=(0.5))
 
-"""
+def choque(foton):
+    v=mag(foton.velocidad)
+    theta=np.random.random()*np.pi
+    phi=np.random.random()*np.pi*2
+    vx=v*np.sin(theta)*np.cos(phi)
+    vy=v*np.sin(theta)*np.sin(theta)
+    vz=v*np.cos(theta)
+    wave_lenghtf=round(380+(200*(1-np.cos(theta))),0)
+    return vector(vx,xy,vz), wave_lenghtf
+
+print(conversion['450'])
+#foton=helix(pos=vector(0,0,0), axis=vector(5,0,0), radius=0.5,color=vector(0,0,0))
+
+#elipse=ellipsoid(pos=vector(2.5,0,0),axis=vector(5,0,0), length=6, height=2, width=2,color=vector(*conversion["700"]),opacity=(0.5),velocidad=vector(3,0,0))
+#print(conversion['380'])
+#print(mag(elipse.velocidad))
+
+dt=0.1
+
+
+
+class photon:
+
+    def __init__ (self, velocidad, posicion,longitud_onda):
+        self.elipse=ellipsoid(pos=posicion,axis=velocidad, length=6, height=2, width=2,color=vector(*conversion[str(longitud_onda)]),opacity=(0.5),velocidad=velocidad)
+        self.velocidad=velocidad
+        self.posicion_resorte=self.elipse.pos - (2.5*self.velocidad/(mag(self.velocidad)))
+        self.axis_resorte=(5*self.velocidad/mag(self.velocidad))
+        self.resorte=helix(pos=self.posicion_resorte, axis=self.axis_resorte, radius=0.5,color=vector(0,0,0))
+    def evolucion_temporal(self,dt):
+        self.elipse.pos=self.elipse.pos+ self.velocidad*dt
+        self.resorte.pos = self.resorte.pos + self.velocidad*dt
+
+prueba=photon(vector(2,0,0), vector(0,0,0), 700)
+prueba2=photon(vector(0,2,0), vector(0,0,0), 400)
+prueba3=photon(vector(0,0,2), vector(0,0,0), 500)
+t=0
+
 while  True:
-    rate(500)
-    if running:
+    rate(30)
+    prueba.evolucion_temporal(dt)
+    prueba2.evolucion_temporal(dt)
+    prueba3.evolucion_temporal(dt)
+    t+=dt
+    if t >10:
+        break
         
-"""
