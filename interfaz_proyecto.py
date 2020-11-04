53a296c00e809ca58c623dab1b2ef7ca2552e953
aherrera3 <angelicaherreraalba@gmail.com>
2020-11-04 01:29:07 -0500

    Separacion interfaz - logica

--- interfaz_proyecto.py
+++ interfaz_proyecto.py
@@ -1,92 +1,18 @@
 
-from abc import ABC, abstractmethod
 import vpython as vp
+import modulo_particulas as mod 
+    
+# electron:
+e1 = mod.Electron(vp.vector(-15,0,0), vp.vector(1,0,0), vp.vector(0.1,1,0.7), 0.0005, 1, "electron") 
 
-vp.scene.background = vp.color.black
-vp.scene.width = 1000 
-vp.scene.height = 600
+# antineutrino
+an1 = mod.AntineutrinoElectronico(vp.vector(5,0,0), vp.vector(1,0,0), vp.vector(0.8,0.5,0.3), 0.00001, 0.5, "antineutrino")  
 
-class Particula(ABC):
-    
-    def __init__(self, posicion, velocidad, nombre):
-        self.posicion=posicion
-        self.nombre=nombre
-        self.velocidad=velocidad
-        self.etiqueta = vp.label(pos=posicion, text=nombre, color=vp.vector(0.1,1,0.7), opacity=0.7, height=15, box=0)
-        
-    #Metodos
-    @abstractmethod
-    def mover():
-        pass
-    
-    #@abstractmethod
-    def crear():
-        pass
-    
-    #@abstractmethod
-    def eliminar():
-        pass
-    
-    #@abstractmethod   
-    def evolucion_temporal(dt):
-        pass    
+t = 0
+dt = 0.1
 
-
-class ParticulaFundamental(Particula,ABC):
-    
-    #Metodo contructor
-    def __init__(self, posicion, velocidad, color, masa, radio, nombre):
-        super().__init__(posicion, velocidad, nombre)
-        self.color=color
-        self.masa=masa
-        self.radio=radio
-        self.esfera= vp.sphere(pos=posicion,radius=radio,color=color, make_trail=True,shininess=0,masa=masa,velocidad=velocidad)
-    
-class ParticulaCompuesta(Particula,ABC):
-    
-    # Metodo constructor
-    def __init__(self, posicion, velocidad, lado, nombre):
-        super().__init__(posicion, velocidad, nombre)                   #hereda el atributo posicion y etiqueta de la superclase
-        self.lado=lado
-        self.apotema=(3)**0.5*lado/6
-        
-    def mover():
-        pass
-        
-class Neutron(ParticulaCompuesta):    
-    
-    def mover(self):
-        print("neutron: ", self.lado, self.posicion)
-
-
-class Electron(ParticulaFundamental):
-    
-    def mover(self):
-        print("electron: ", self.etiqueta)
-        
-        
-    def evolucion_temporal(self, dt):
-        self.posicion = vp.vector(self.posicion + self.velocidad*dt)      # actualiza la posicion
-        
-        
-def main():
-    
-    t = 0
-    dt = 0.01
-    
-    # electron:
-    vel_ini = vp.vector(1,0,0)
-    pos_ini = vp.vector(-15,0,0)
-    color = vp.vector(0.1,1,0.7)
-    
-    e1 = Electron(pos_ini, vel_ini, color, 0.0005, 1, "electron") 
-    
-    while t<10:
-        vp.rate(500)
-        e1.evolucion_temporal(dt)
-        
-        t+=dt
-        
-        
-if __name__ == "__main__":
-    main()
+while t<10:
+    vp.rate(500)
+    e1.evolucion_temporal(dt)
+    an1.evolucion_temporal(dt)    
+    t+=dt
