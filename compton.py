19e69d3e519472cce49ae89235eece30e354e002
David <jd.salamancac>
2020-11-03 23:28:46 -0500

    foton

--- compton.py
+++ compton.py
@@ -36,9 +36,47 @@ def estado_inicial():
     global running
     #running=False
 
-foton=helix(pos=vector(0,0,0), axis=vector(5,0,0), radius=0.5,color=vector(250,250,0))
 
-elipse=ellipsoid(pos=vector(2.5,0,0), axis=vector(5,0,0), length=5, height=1, width=1,color=vector(250,250,0))
+
+def longitud_de_onda(l):
+    r=0.0 
+    g=0.0
+    b=0.0
+
+    if ((l>=400.0) and (l<410.0)):
+        t=(l-400.0)/(410.0-400.0)
+        r+=(0.33*t)-(0.20*t*t)
+    elif ((l>=410.0) and (l<475.0)):
+        t=(l-410.0)/(475.0-410.0)
+        r=0.14-(0.13*t*t)
+    elif ((l>=545.0) and (l<595.0)):
+        t=(l-545.0)/(595.0-545.0)
+        r=(1.98*t)-(t*t)
+    elif ((l>=595.0) and (l<650.0)):
+        t=(l-595.0)/(650.0-595.0) 
+        r=0.98+(0.06*t)-(0.40*t*t)
+    elif ((l>=650.0) and (l<700.0)):
+        t=(l-650.0)/(700.0-650.0)
+        r=0.65-(0.84*t)+(0.20*t*t)
+    if ((l>=415.0) and (l<475.0)):
+        t=(l-415.0)/(475.0-415.0) 
+        g=(0.80*t*t)
+    elif ((l>=475.0) and (l<590.0)):
+        t=(l-475.0)/(590.0-475.0)
+        g=0.8 +(0.76*t)-(0.80*t*t)
+    elif ((l>=585.0) and (l<639.0)): 
+        t=(l-585.0)/(639.0-585.0)
+        g=0.84-(0.84*t)
+    if ((l>=400.0) and (l<475.0)):
+        t=(l-400.0)/(475.0-400.0)
+        b=(2.20*t)-(1.50*t*t)
+    elif ((l>=475.0) and(l<560.0)) :
+        t=(l-475.0)/(560.0-475.0)
+        b=0.7-t+(0.30*t*t)
+    return r,g,b
+foton=helix(pos=vector(0,0,0), axis=vector(5,0,0), radius=0.5,color=vector(0,0,0))
+
+elipse=ellipsoid(pos=vector(2.5,0,0),axis=vector(5,0,0), length=6, height=2, width=2,color=vector(250,0,0),opacity=(0.5))
 
 """
 while  True:
